915780b313a246af5a795939b71dcf2ebe014775
Sebastian Triemer <sebastian.triemer@outlook.de>
2022-05-05 09:59:33 +0200

    implemented createBoard function

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -13,6 +13,29 @@ class TicTacToe {
   checkForWin() {}
   //Method that resets the board to the beginning
   reset() {}
+  createBoard() {
+    //creates a 2 dimensional array to better visualize the check for win implementation
+    let board2d = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    //Returns a node list, needs to be converted to array to use array methods
+    let board = document.querySelectorAll(".tile");
+    //Converts the node list to an array
+    let boardArray = Array.from(board);
+    //Fills the 2 dimensional array with all the tiles ([0][0]=tile1, [0][1]=tile2...)
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        board2d[i][j] = boardArray.shift();
+        //Adds the event listener to each tile
+        board2d[i][j].addEventListener("click", function () {
+          console.log("clicked");
+        });
+      }
+    }
+    return board2d;
+  }
 }
 
 //create the game object from TicTacToe
